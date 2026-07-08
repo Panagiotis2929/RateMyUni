d6ed9d38023e4c35df2e98326f9b135d8803f7bf
Panagiotis2929 <rpkyranas@gmail.com>
2026-07-08 23:54:19 +0300

    g

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 import { config } from './config.js';
 const SUPABASE_URL = config.SUPABASE_URL;
-const SUPABASE_KEY = config.SUPABASE_KEY;
 
 function initSupabase() {
   const url = (typeof SUPABASE_URL !== 'undefined' && SUPABASE_URL && !SUPABASE_URL.startsWith('ΒΑΛΕ'))
